b11060b1b17ecf673ef7f19ace96528ab63bcb97
Dmitry Danilov <danilov.inform@yandex.ru>
2021-06-27 20:07:03 +0300

    fix styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ import { ContainerComponent } from './shared/container/container.component';
 
 const routes: Routes = [
     { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then((module) => module.DashboardModule)},
-    { path: 'bookmarks', loadChildren: () => import('./bookmarks/bookmarks.module').then((module) => module.BookmarksModule)}
+    { path: 'bookmarks', loadChildren: () => import('./bookmarks/bookmarks.module').then((module) => module.BookmarksModule)},
+    { path: '**', redirectTo: '/dashboard'},
 ];
 
 @NgModule({
